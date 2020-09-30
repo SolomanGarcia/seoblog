fddc80291fc8bf234f0572ec05fa738cb7451b3f
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-30 15:08:53 -0600

    create Signin component

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -15,3 +15,18 @@ export const signup = (user) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const signin = (user) => {
+  return fetch(`http://localhost:8000/api/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
