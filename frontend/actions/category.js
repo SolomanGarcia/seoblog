cbd41a63022648318668405b0ca4454dc3d150f4
Soloman Garcia <soloman.garcia@gmail.com>
2020-10-02 15:20:00 -0600

    delete category

--- frontend/actions/category.js
+++ frontend/actions/category.js
@@ -16,3 +16,38 @@ export const create = (category, token) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const getCategories = () => {
+  return fetch(`http://localhost:8000/api/categories`, {
+    method: "GET"
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const singleCategory = (slug) => {
+  return fetch(`http://localhost:8000/api/category/${slug}`, {
+    method: "GET"
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const removeCategory = (slug, token) => {
+  return fetch(`http://localhost:8000/api/category/${slug}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
